9d118a7555ab283f5f7d709987dc7ed7ddecf0f3
janney23 <sas2323@gmail.com>
2021-08-25 14:35:46 -0400

    Completed module 5

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pluralsight.blog.data.PostRepository;
@@ -23,6 +24,13 @@ public class BlogController {
 		List<Post> postList = postRepository.getAllPosts();
 		modelMap.put("posts", postList);
 		return "home";
+	}
+	
+	@RequestMapping("/post/{id}")
+	public String postDetails(@PathVariable Long id, ModelMap modelMap) {
+		Post post = postRepository.findById(id);
+		modelMap.put("post", post);
+		return "post-details";
 	}
 
 }
